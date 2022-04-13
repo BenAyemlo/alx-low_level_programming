afef692ad935173e37213b2a9c8f63f96c42fd12
BenAyemlo-97 <benayemloharuna@gmail.com>
2022-04-12 21:03:21 -0700

    My task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -19,12 +19,12 @@ _putchar(' ');
 prod = num * mult;
 
 if (prod <= 9)
-_puchar(' ');
+_putchar(' ');
 else
 _putchar((prod / 10) + '0');
 
 _putchar((prod % 10) + '0');
 }
-_puchar('\n');
+_putchar('\n');
 }
 }
