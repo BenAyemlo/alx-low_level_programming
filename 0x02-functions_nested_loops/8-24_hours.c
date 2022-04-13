94d69de09af45cfaa4371b5cf575d21e646e5cf0
BenAyemlo-97 <benayemloharuna@gmail.com>
2022-04-12 20:42:10 -0700

    My task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * Jack_bauer - Prints every minute of the day of
- * 		Jack Bauer, starting from 00:00 to 23:59.
+ *jack_bauer - Prints every minute of the day of
+ * 	       jack Bauer, starting from 00:00 to 23:59.
  */
 void jack_bauer(void)
 {
@@ -21,4 +21,3 @@ _putchar('\n');
 }
 }
 }
-
