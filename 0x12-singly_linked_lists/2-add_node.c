650b1ed4996807b1cd8fe7e39ed2f3da78d5d85c
BenAyemlo-97 <benayemloharuna@gmail.com>
2022-05-26 13:09:16 -0700

    My task 2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -29,3 +29,21 @@ return (temp);
 }
 return (0);
 }
+
+/**
+ * _strlen - Return the length of a string
+ * @s: String to count
+ *
+ * Return: String length
+ */
+int _strlen(const char *s)
+{
+int c = 0;
+
+while (*s)
+{
+s++;
+c++;
+}
+return (c);
+}
