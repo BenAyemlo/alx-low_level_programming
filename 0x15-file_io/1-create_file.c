22a522d92e94b2f96bf0ffef8f7b0edcf3b46420
BenAyemlo-97 <benayemloharuna@gmail.com>
2022-06-01 09:44:57 -0700

    My task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -24,3 +24,19 @@ write(fd, text_content, _strlen(text_content));
 close(fd);
 return (1);
 }
+
+/**
+ * _strlen - Returns the length of a string
+ * @s: String to count
+ *
+ * Return: String length
+ */
+int _strlen(char *s)
+{
+int c = 0;
+
+while (s[c])
+c++;
+
+return (c);
+}
